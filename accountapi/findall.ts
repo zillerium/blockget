43152298368b040e1b42f3a4b151fa5f48baccdf
Trevor Oakley <sales@merrows.co.uk>
2019-07-02 06:35:28 +0000

    counters

--- accountapi/findall.ts
+++ accountapi/findall.ts
@@ -5,6 +5,14 @@ import { DCoreSdk, ChainObject } from "dcorejs-sdk";
 const api = DCoreSdk.createForHttp({ baseUrl: "https://testnet-api.dcore.io/" })
 // get account by name, resolves to account id '1.2.27'
 let searchTerm: string = "account";
-const all = api.accountApi.findAll(searchTerm, undefined, undefined, 1)
-                .subscribe(result => console.log(result))
+const all = api.accountApi.findAll(searchTerm, undefined, undefined, 4)
+    .subscribe(result => {
+    let i: number = 0;
+    while (i < 4) {
+        console.log(result[i].id.instance);
+	i++;
+	console.log("***********************");
+    }
+    })
+
 
--- accountapi/getall.ts
+++ accountapi/getall.ts
@@ -5,6 +5,11 @@ import { DCoreSdk, ChainObject } from "dcorejs-sdk";
 const api = DCoreSdk.createForHttp({ baseUrl: "https://testnet-api.dcore.io/" })
 // get account by name, resolves to account id '1.2.27'
 let accounts: ChainObject[] = [ChainObject.parse("1.2.27"), ChainObject.parse("1.2.28")]
-const all = api.accountApi.getAll(accounts)
-                .subscribe(result => console.log(result))
+let i: number = 0;
+
+while (i < 4) {
+    const all = api.accountApi.getAll(accounts)
+                .subscribe(result => console.log(result[1].id))
+    i++;
+}
 
