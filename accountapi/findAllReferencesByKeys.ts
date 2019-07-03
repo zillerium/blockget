4e68ed2a426b806b7c0850d76335314366db4587
Trevor Oakley <sales@merrows.co.uk>
2019-07-03 19:11:15 +0000

    keys

--- accountapi/findAllReferencesByKeys.ts
+++ accountapi/findAllReferencesByKeys.ts
@@ -5,7 +5,7 @@ import { DCoreSdk, Address } from "dcorejs-sdk";
 // create the API
 const api = DCoreSdk.createForHttp({ baseUrl: "https://testnet-api.dcore.io/" })
 // get account by name, resolves to account id '1.2.27'
-let address: Address = Address.parse("DCT6TjLhr8uESvgtxrbWuXNAN3vcqzBMw5eyEup3PMiD2gnVxeuTb");
-const disposable = api.accountApi.get(address)
+let address: Address[] = [Address.parse("DCT6TjLhr8uESvgtxrbWuXNAN3vcqzBMw5eyEup3PMiD2gnVxeuTb")];
+const disposable = api.accountApi.findAllReferencesByKeys(address)
     .subscribe((account) => console.log(account));
 
